b5bf6106a12b8e50b7591c63f15d57fe36d8d311
zj <231250960@qq.com>
2019-04-08 17:57:46 +0800

    param

--- api/v1/api.js
+++ api/v1/api.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
+
 /* GET home page. */
-router.get('/user/:id', function(req, res, next) {
-    res.send(req.query)
-    console.log(req.query)
+router.get('/user/:id', function (req, res, next) {
+    //res.send(req.query)
+    let course = courses.find(
+        courses => courses.id == parseInt(req.params.id)
+    );
+    res.send(course);
 });
 
 module.exports = router;
