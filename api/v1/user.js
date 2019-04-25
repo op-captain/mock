904fb40603e26e8a1f243112883acac5066c2bc9
zj <231250960@qq.com>
2019-04-25 11:25:35 +0800

    c

--- api/v1/user.js
+++ api/v1/user.js
@@ -10,7 +10,6 @@ router.post('/', function (req, res, next) {
     // 获取前台页面传过来的参数
     var param = req.body;
 
-    console.log(param)
     
     // 向前台返回JSON方法的简单封装
     var jsonWrite = function (res, ret) {
@@ -23,6 +22,8 @@ router.post('/', function (req, res, next) {
             res.json(ret);
         }
     };
+
+    
 
     curdUser.add(param,res,next,jsonWrite)
 
--- crud/user.js
+++ crud/user.js
@@ -11,15 +11,14 @@ var pool  = mysql.createPool(_.extend({}, dbconf.mysql));
 
 module.exports = {
 	add: function (param, res, next,callbackResponse) {
-        console.log(param)
+
 		pool.getConnection(function(err, connection) {
 			var hash = utils.hash()
-            console.log(hash)
+
  
 			// 建立连接，向表中插入值
-			// 'INSERT INTO user(id, name, age) VALUES(0,?,?)',
-			connection.query(sql.insert, [hash, param.name, param.sex, param.age,], function(err, result) {
-                console.log(result)
+			connection.query(sql.insert, [hash, param.name, param.sex, param.age, "http://tx.haiqq.com/uploads/allimg/170506/0G9454641-7.jpg"], function(err, result) {
+
 				if(result) {
 					result = {
 						code: 200,
