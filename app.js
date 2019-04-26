32f75ea80b2f90915a601ae40ffb3971605d21ec
zj <231250960@qq.com>
2019-04-26 09:49:46 +0800

    c

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // 允许所有的请求形式
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //res.header("Access-Control-Allow-Origin", "*");
+    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
